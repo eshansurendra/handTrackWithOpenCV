2cf0b66d71090ee259d88bf6b17df7d5a5138cab
Januka Eshan Surendra <107266456+eshansurendra@users.noreply.github.com>
2024-04-20 16:45:20 +0530

    Update handTrack.py

--- handTrack.py
+++ handTrack.py
@@ -4,18 +4,23 @@ from cvzone import HandTrackingModule
 cap = cv2.VideoCapture(0)
 detector = HandTrackingModule.HandDetector()
 
-while (True):
+while True:
     success, img = cap.read()
     detector.findHands(img)
-    lmList, bboxInfo = detector.findPosition(img)
+    lmList, _ = detector.findPosition(img)
 
     if lmList:
-        # Check for gestures
-        # Example: Check for thumbs up gesture
-        if lmList[4][1] < lmList[3][1]:  # Thumb tip above index finger tip
+        # Gesture recognition: Thumbs up
+        if lmList[4][1] < lmList[3][1] and lmList[8][2] < lmList[6][2]:  # Thumb tip above index finger tip and thumb tip below wrist
             cv2.putText(img, "Thumbs Up", (lmList[4][1], lmList[4][2]), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
 
-        # Add more gesture checks here
+        # Gesture recognition: Peace sign
+        if lmList[6][2] < lmList[8][2] and lmList[10][2] < lmList[8][2]:  # Index and middle finger tips above thumb tip
+            cv2.putText(img, "Peace Sign", (lmList[8][1], lmList[8][2]), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+
+        # Gesture recognition: OK sign
+        if lmList[4][1] < lmList[3][1] and lmList[8][1] < lmList[7][1]:  # Thumb tip above index finger tip and index finger tip to the left of thumb tip
+            cv2.putText(img, "OK Sign", (lmList[8][1], lmList[8][2]), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
