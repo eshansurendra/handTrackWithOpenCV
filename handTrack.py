d5d14b864fbc565d9275d5e91f52775fa16cfbac
Januka Eshan Surendra <107266456+eshansurendra@users.noreply.github.com>
2024-04-20 16:40:52 +0530

    Add files via upload

---
+++ handTrack.py
@@ -0,0 +1,10 @@
+import cv2
+from cvzone import HandTrackingModule
+cap = cv2.VideoCapture(0)
+detector = HandTrackingModule.HandDetector()
+
+while (True):
+    success, img = cap.read()
+    detector.findHands(img)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
